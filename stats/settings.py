eecc609feac3553d48af5e088189d1e09221401a
MagpieRook <alex@alexrook.com>
2018-06-27 19:15:59 -0400

    Settings changes

--- stats/settings.py
+++ stats/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'keepitsecret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['localhost']
 
